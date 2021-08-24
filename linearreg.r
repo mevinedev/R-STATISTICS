40b8edfea8d4bc24538ebef38afa3dd123a67322
Mevine  Akinyi <72660182+mevinedev@users.noreply.github.com>
2021-08-24 17:14:51 +0300

    Update linearreg.r

--- linearreg.r
+++ linearreg.r
@@ -1,4 +1,4 @@
-
+# linear regression 
 # Create the predictor and response variable.
 y <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
 x <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
@@ -13,7 +13,8 @@ result <-  predict(relation,a)
 print(result)
 
 # illustration of regression line.
-#plot regression line. 
+# plot regression line. 
 plot(y~x)
 abline(lm(y~x))
       
+# the result above shows linear regression for two variables. 
