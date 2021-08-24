44d34036612604711f88464153b00c217a128196
Mevine  Akinyi <72660182+mevinedev@users.noreply.github.com>
2021-08-24 17:13:04 +0300

    modified predictor variable and response variable
    added example for prediction

--- linearreg.r
+++ linearreg.r
@@ -1,12 +1,19 @@
 
 # Create the predictor and response variable.
-x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
-y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
+y <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+x <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
 relation <- lm(y~x)
 print(relation)
 print(summary(relation))
 
-#plot regression line 
-plot(x~y)
-abline(lm(x~y))
+# example of prediction.
+# Find xwhere y is 170.
+a <- data.frame(y = 170)
+result <-  predict(relation,a)
+print(result)
+
+# illustration of regression line.
+#plot regression line. 
+plot(y~x)
+abline(lm(y~x))
       
