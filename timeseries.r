0e38107a58c307a54dbb4634beddab2739bc9f71
Mevine  Akinyi <72660182+mevinedev@users.noreply.github.com>
2021-08-25 19:48:30 +0300

    multiple time series

---
+++ timeseries.r
@@ -0,0 +1,22 @@
+
+# Get the data points in form of a R vector.
+rainfall1 <- c(799,1174.8,865.1,1334.6,635.4,918.5,685.5,998.6,784.2,985,882.8,1071)
+rainfall2 <- 
+  c(655,1306.9,1323.4,1172.2,562.2,824,822.4,1265.5,799.6,1105.6,1106.7,1337.8)
+
+# Convert them to a matrix.
+combined.rainfall <-  matrix(c(rainfall1,rainfall2),nrow = 12)
+
+# Convert it to a time series object.
+rainfall.timeseries <- ts(combined.rainfall,start = c(2012,1),frequency = 12)
+
+# Print the timeseries data.
+print(rainfall.timeseries)
+
+# Give the chart file a name.
+png(file = "rainfall_combined.png")
+
+# Plot a graph of the time series.
+plot(rainfall.timeseries, main = "Multiple Time Series")
+
+# Save the file.
